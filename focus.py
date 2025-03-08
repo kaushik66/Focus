e90372f5a27b654c01e2451c45b7d869644c3bd4
Kaushik Das <kaushik.kd2005@gmail.com>
2025-03-09 02:24:54 +0530

    1st commit

--- focus.py
+++ focus.py
@@ -13,8 +13,8 @@ from waitress import serve
 from dotenv import load_dotenv
 from markdown2 import markdown
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.llms.ollama import Ollama
+from langchain.embeddings import HuggingFaceEmbeddings
+import ollama
 import fitz
 import requests
 import time
@@ -171,10 +171,10 @@ def chat():
         documents = SimpleDirectoryReader("data").load_data()
 
         # bge-base embedding model
-        Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+        Settings.embed_model = HuggingFaceEmbeddings(model_name="BAAI/bge-base-en-v1.5")
 
         # ollama
-        Settings.llm = Ollama(model="llama3", request_timeout=360.0)
+        Settings.llm = ollama(model="llama3", request_timeout=360.0)
 
         index = VectorStoreIndex.from_documents(
             documents,
